8afeb38c5fd12fe7946a3e3da1c75d6cfe1a0944
Kovalski Pingvin <golubgolubovic@yandex.com>
2025-01-07 14:24:02 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def coo_console_log_print(message):
+  """Prints a message to the console.
+
+  Args:
+    message: The message to be printed.
+  """
+  print(message)
+
+# Example usage
+coo_console_log_print("Hello World!")
